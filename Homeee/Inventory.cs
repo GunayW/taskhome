c252e06c33d4fce7a11de12751d6a0850a4730bf
GunayW <gunayeh-bpa202@code.edu.az>
2025-11-04 13:26:17 +0400

    Task Added

---
+++ Homeee/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task9
+{
+    internal class Inventory
+    {
+        private Item[] _items =new Item[10];
+
+        public Item this[int index]
+        {
+            get
+            {
+               return _items[index];
+            }
+            set
+            {
+                _items[index] = value;
+            }
+            }
+        public int Count => _items.Length;
+
+        public Inventory(Item[] items)
+        {
+            _items = items;
+        }
+
+        public Inventory()
+        {
+        }
+
+        public void ShowAll()
+        {
+            foreach (var item in _items)
+            {
+                Console.WriteLine($" {item.Name}, {item.Type},  {item.Value}, {item.IsUsed}");
+            }
+        }
+    }
+}
---
+++ Homeee/Program.cs
@@ -0,0 +1,22 @@
+﻿using Task9;
+
+Item silah1=new ("AKM","Weapon",800,true);
+Item silah2=new ("M416","Weapon",900,true);
+Item silah3=new ("M24","Weapon",1200,false);
+Item silah4=new ("AVM","Weapon",1300,false);
+
+GameStats.RegisterItem(silah1);
+GameStats.RegisterItem(silah2);
+GameStats.RegisterItem(silah3);
+GameStats.RegisterItem(silah4);
+
+
+object obj = silah2;
+Inventory playerinventory = new Inventory();
+playerinventory[0] = silah3;
+playerinventory[0].Use();
+Item silah5 = (Item)obj;
+IUsable silah6 = silah4;
+silah6.Use();
+ITradable silah7 = silah3;
+silah7.Trade();
